e35cfc3a36ca5a1f09a299c00cf32b7d4e1fff7a
Kit_bryan <81080843+Kit-Bryan@users.noreply.github.com>
2022-12-17 23:34:56 +0800

    Add files via upload
    Reuploaded

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import ReactDOM from "react-dom/client";
+
+export default function Footer () {
+  return (
+    <footer class="footer">
+      <button>
+        <a href="">
+          <i class="fa-brands fa-square-twitter"></i>
+        </a>
+      </button>
+      <button>
+        <a href="">
+          <i class="fa-brands fa-square-facebook"></i>
+        </a>
+      </button>
+      <button>
+        <a href="">
+          <i class="fa-brands fa-square-instagram"></i>
+        </a>
+      </button>
+      <button>
+        <a href="https://github.com/kit-bryan">
+          <i class="fa-brands fa-github"></i>
+        </a>
+      </button>
+    </footer>
+  );
+}
---
+++ src/components/Portrait.jsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+
+export default function Portrait() {
+  return (
+    <div class="portrait">
+      <img
+        class="portrait-img"
+        src="https://images.unsplash.com/photo-1499557354967-2b2d8910bcca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1036&q=80"
+        alt="portrait"
+      />
+    </div>
+  );
+}
